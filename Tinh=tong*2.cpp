1ed02d7e9cc5372af15d9ad51357666848502ddf
anhtran123-max <75102477+anhtran123-max@users.noreply.github.com>
2021-06-29 04:45:06 +0700

    Update Tinh=tong*2.cpp

--- Tinh=tong*2.cpp
+++ Tinh=tong*2.cpp
@@ -1,25 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 int main(){
-    int N,Ngoc,i,tong=0,tich=1;
-    do{
-        printf("N=");
-        scanf("%d",&N);
-        if(N<10 && N>99)
-            printf("Gia tri cua N khong hop le.Xin moi nhap lai.\n");
+    int i, a,b;
+    for(i=10 ; i<100 ; i++){
+        a=i%10;
+        b=i/10;
+        if(a*b==2*(a+b))
+            printf("%d\n",i);
     }
-    while(N<10 && N>99);
-    Ngoc=N;
-    while(N!=0)
-    {
-        i=N%10;
-        tong=tong+i;
-        tich=tich*i;
-        N=N/10;
-    }
-    if(tich==2*tong)
-        printf("%d la gia tri can tim",Ngoc);
-    else 
-        printf("%d khong phai gia tri can tim",Ngoc);
-    getch();
 }
