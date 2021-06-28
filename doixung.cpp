f0ebdcebd9aa1d9ed16db4198c6f87f6703eba15
anhtran123-max <75102477+anhtran123-max@users.noreply.github.com>
2021-06-28 22:50:24 +0700

    Create doixung.cpp

---
+++ doixung.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+bool IsNumber(char s[], int n){
+    if(n == 0) return false;
+    if(s[0] == '0') return false;
+    for(int i = 0; i < n;i++){
+        if(s[i] < '0' || s[i] > '9') return false;
+    }
+    return true;
+}
+int main(){
+    char s[100];
+    gets(s);
+    int n = strlen(s);
+    if(IsNumber(s, n) == false){
+        printf("\nBan nhap sai dinh dang!");
+        return 0;
+    }
+    bool isPar = true;
+    int i = 0, j = n - 1;
+    while(i < j){
+        if(s[i] != s[j]){
+            isPar = false;
+            break;
+        }else{
+            ++i; 
+            --j; 
+        }
+    }
+    if(isPar == true){
+        printf("%s la so doi xung!", s);
+    }else{
+        printf("%s khong la so doi xung!", s);
+    }
+}
