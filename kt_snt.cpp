9fd1b5ab9952d1a468500990bdb934f9a78272c7
anhtran123-max <75102477+anhtran123-max@users.noreply.github.com>
2021-06-30 22:01:26 +0700

    Update kt_snt.cpp

--- kt_snt.cpp
+++ kt_snt.cpp
@@ -1,27 +1,30 @@
-#include <stdio.h>
-#include <math.h>
- 
-int main(){
-    int n;
-    do{
-    printf("\nNhap n = ");
-    scanf("%d", &n);
-    if(n<=0)
-        printf("\nNhap so khong hop le!!!");  
-    }while (n<=0);
-    if(n < 2){
-        printf("\n%d khong phai so nguyen to", n);
-        return 0;
-    }
-    int count = 0;
-    for(int i = 2; i <= sqrt(n); i++){
-        if(n % i == 0){
-            count++;
-        }
-    }
-    if(count == 0){
-        printf("\n%d la so nguyen to", n);
-    }else{
-        printf("\n%d khong phai so nguyen to", n);
-    }
+#include <iostream>
+#include<math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int isPrimeNumber(int n);
+int main(int argc, char** argv) {
+	int n;
+	cout << "\nNhap n: ";
+	cin>>n;
+	cout<<"\nCac so nguyen to la: ";
+	for(int i=0; i<=n;i++){
+		if(isPrimeNumber(i))
+		    cout<<i<<" ";
+	}
+	return 0;
+}
+int isPrimeNumber(int n){
+	if(n<2){
+		return 0;
+	}
+	int squareRoot= sqrt(n);
+	for(int i=2;i<=squareRoot;i++){
+		if(n%i==0){
+			return 0;
+		}
+	}
+	return 1;
 }
