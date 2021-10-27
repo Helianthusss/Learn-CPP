4e8129802049d2eba6afcb4c55adc54f6114ed46
DESKTOP-DN5FP91\ASIA <anhtrankoc@gmail.com>
2021-10-27 18:00:54 +0700

    :>

---
+++ .history/C++/bai tap/bai tap/bai1_20210817194017.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void NhapMang(int a[], int n){
+	for(int i=0; i<n; i++){
+			cout<<"\na["<<i<<"]= ";
+			cin>>a[i];
+	   }
+}
+void XuatMang(int a[], int n){
+	for(int i=0;i<n;i++){
+			cout<<"\na["<<i<<"]="<<a[i];
+		}
+}
+void sapXep(int a[], int n){
+    int temp;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i] > a[j]){
+                 temp = a[i];
+                 a[i] = a[j];
+                 a[j] = temp;
+            }
+        }
+        cout << a[i] <<" ";
+    }
+}
+int main(){
+    int a[100] ,n;
+    cout<< "\n Nhap n: ";
+    cin >> n;
+    cout<< "\n Nhap mang!!!";
+    NhapMang(a,n);
+    cout <<"\n Xuat mang!!!";
+    XuatMang(a,n);
+    cout << "\n Sap xep mang tang dan!!!\n";
+    sapXep(a,n);
+    return 0;
+}
---
+++ HTML/practice/script.js
@@ -0,0 +1,11 @@
+// // console.log('tadaa');
+// var x=1,y=2;
+// console.log(x+y);
+
+// chaythu= function(){
+// 	console.log('ditmemay');
+// }
+var diem=4;
+if(diem==4) {
+	console.log('trung binh');
+}
