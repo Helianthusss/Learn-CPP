6067d01b6a723b96c879d42a1bfe4f05cace2e40
DESKTOP-DN5FP91\ASIA <anhtrankoc@gmail.com>
2022-01-30 10:03:21 +0700

    update

--- HTML/11-3/pt1.js
+++
@@ -1,7 +0,0 @@
-function eQuation1(){
-    let a = document.getElementById("number_a").value;
-    let b = document.getElementById("number_b").value;
-    let c = document.getElementById("number_c").value;
-    let x = (parseInt(c)-parseInt(b))/parseInt(a);
-    document.getElementById("result").innerHTML = "Solution of the equation of degree 1" + " = " + x;
-}
